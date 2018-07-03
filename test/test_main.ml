18455c4cf942b6d00f3591ebf8c9137a41c7a0ba
Milo Turner <iitalics@gmail.com>
2018-07-02 21:50:57 -0700

    Posn test

--- test/test_main.ml
+++ test/test_main.ml
@@ -1,6 +1,35 @@
 
+module Posn = struct
+  type t =
+    { x : int
+    ; y : int }
+      [@@generate_lens]
+
+  let to_string { x ; y } =
+    Printf.sprintf "{%d,%d}" x y
+
+  let bounce_left_wall : t -> t =
+    update_x ~f:abs
+
+  (* *)
+
+  let assert_ ~ctxt a b =
+    OUnit2.assert_equal ~ctxt ~printer:to_string
+      a b
+
+  let test =
+    let open OUnit2 in
+    "Posn tests" >:::
+      [ "bounce_left_wall" >::
+          begin fun ctxt ->
+          assert_ ~ctxt
+            (bounce_left_wall { x = -2 ; y = 4 })
+            { x = 2 ; y = 4 }
+          end
+      ]
+end
+
 let () =
   let open OUnit2 in
   run_test_tt_main
-  @@ test_list
-       []
+  @@ Posn.test
