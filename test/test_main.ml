63484eada78979638b205a48805d32230c2618f0
Milo Turner <iitalics@gmail.com>
2018-07-02 22:40:30 -0700

    Different test

--- test/test_main.ml
+++ test/test_main.ml
@@ -4,43 +4,38 @@ module Posn = struct
     { x : int
     ; y : int }
       [@@lens.no_prefix]
+      [@@lens.no_get]
       [@@lens.generate]
-
-  let to_string { x ; y } =
-    Printf.sprintf "{%d,%d}" x y
 
   let bounce_left_wall : t -> t =
     update_x ~f:abs
 
-  (* *)
-
-  let assert_ ~ctxt a b =
-    OUnit2.assert_equal ~ctxt ~printer:to_string
-      a b
-
-  let test =
-    let open OUnit2 in
-    "Posn tests" >:::
-      [ "bounce_left_wall" >::
-          begin fun ctxt ->
-          assert_ ~ctxt
-            (bounce_left_wall { x = -2 ; y = 4 })
-            { x = 2 ; y = 4 }
-          end
-      ]
 end
 
 type ('a, 'b, 'c) trip =
   { fst : 'a ; snd : 'b ; thd : 'c }
     [@@lens.prefix_from_type]
-    [@@lens.update_prefix "modify"]
-    [@@lens.no_named_arg]
     [@@lens.generate]
 
-let foo : (int, 'a, 'b) trip -> (string, 'a, 'b) trip
-  = modify_trip_fst string_of_int
+let ignore1 t = set_trip_fst () t
 
 let () =
   let open OUnit2 in
   run_test_tt_main
-  @@ Posn.test
+  @@ test_list
+       [ "bounce_left_wall" >::
+           begin fun ctxt ->
+           Posn.(
+             assert_equal ~ctxt
+               { x = 2 ; y = 4 }
+               (bounce_left_wall { x = -2 ; y = 4 }))
+           end
+
+       ; "succ1" >::
+           begin fun ctxt ->
+           let p0 = { fst = 10 ; snd = 0 ; thd = 1 } in
+           let p1 = { fst = () ; snd = 0 ; thd = 1 } in
+           assert_equal ~ctxt
+             p1
+             (ignore1 p0)
+           end ]
