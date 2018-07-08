da46ce9ffbd4d3d97f4ef66c4f56c3b3623a4731
Milo Turner <iitalics@gmail.com>
2018-07-07 19:25:07 -0700

    Added a few more simple operations

--- lib/lenslib.ml
+++ lib/lenslib.ml
@@ -1,23 +1,22 @@
 
-type ('s, 't, 'a, 'b) lens =
-  ('s -> 'a) * ('b -> 's -> 't)
-type ('s, 'a) lens' =
-  ('s, 's, 'a, 'a) lens
+type ('s, 't, 'a, 'b) lens = ('s -> 'a) * ('b -> 's -> 't)
+type ('s, 'a) lens' = ('s, 's, 'a, 'a) lens
 
 let _1 = fst, fun x (_, y) -> x, y
 let _2 = snd, fun y (x, _) -> x, y
 
-let[@ocaml.inline] view lns s : 'a =
-  fst lns s
-let[@ocaml.inline] set lns s x : 't =
-  snd lns x s
-let[@ocaml.inline] over lns ~(f : 'a -> 'b) s : 't =
-  snd lns (f (fst lns s)) s
+let[@ocaml.inline] lens ~view ~set = view, set
+let[@ocaml.inline] view lns s = fst lns s
+let[@ocaml.inline] set lns s x = snd lns x s
+let[@ocaml.inline] over lns ~f s = snd lns (f (fst lns s)) s
 let[@ocaml.inline] compose lns1 lns2 =
   (fun s -> view lns2 (view lns1 s)),
   (fun y s -> set lns1 s (set lns2 (view lns1 s) y))
 
 module Infix = struct
-  let[@ocaml.inline] (^.) s lns = view lns s
+  let (^.) s lns = view lns s
+  let (^~) lns x s = set lns s x
+  let (^%) lns f s = over lns ~f s
   let (^>) = compose
+  let (^<) m n = compose n m
 end
--- lib/lenslib.mli
+++ lib/lenslib.mli
@@ -5,6 +5,7 @@ type ('s, 'a) lens' = ('s, 's, 'a, 'a) lens
 val _1 : ('x * 'a, 'y * 'a, 'x, 'y) lens
 val _2 : ('a * 'x, 'a * 'y, 'x, 'y) lens
 
+val lens    : view:('s -> 'a) -> set:('b -> 's -> 't) -> ('s, 't, 'a, 'b) lens
 val view    : ('s, _ , 'a, _ ) lens -> ('s -> 'a)
 val set     : ('s, 't, _ , 'b) lens -> ('s -> 'b -> 't)
 val over    : ('s, 't, 'a, 'b) lens -> f:('a -> 'b) -> ('s -> 't)
@@ -13,8 +14,22 @@ val compose : ('ss, 'tt, 's, 't) lens
               -> ('ss, 'tt, 'a, 'b) lens
 
 module Infix : sig
+  (** view *)
   val (^.) : 's -> ('s, _, 'a, _) lens -> 'a
+
+  (** set *)
+  val (^~) : ('s, 't, _, 'b) lens -> 'b -> 's -> 't
+
+  (** over *)
+  val (^%) : ('s, 't, 'a, 'b) lens -> ('a -> 'b) -> ('s -> 't)
+
+  (** compose *)
   val (^>) : ('ss, 'tt, 's, 't) lens
              -> ('s, 't, 'a, 'b) lens
+             -> ('ss, 'tt, 'a, 'b) lens
+
+  (** compose (flipped) *)
+  val (^<) : ('s, 't, 'a, 'b) lens
+             -> ('ss, 'tt, 's, 't) lens
              -> ('ss, 'tt, 'a, 'b) lens
 end
