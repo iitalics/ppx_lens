6caf7487f4db1d0fad6ea1e0796214594ffbcaa4
Milo Turner <iitalics@gmail.com>
2018-07-07 19:21:34 -0700

    fixup: indent ":"s

--- lib/lenslib.mli
+++ lib/lenslib.mli
@@ -5,9 +5,9 @@ type ('s, 'a) lens' = ('s, 's, 'a, 'a) lens
 val _1 : ('x * 'a, 'y * 'a, 'x, 'y) lens
 val _2 : ('a * 'x, 'a * 'y, 'x, 'y) lens
 
-val view  : ('s, _ , 'a, _ ) lens -> ('s -> 'a)
-val set   : ('s, 't, _ , 'b) lens -> ('s -> 'b -> 't)
-val over  : ('s, 't, 'a, 'b) lens -> f:('a -> 'b) -> ('s -> 't)
+val view    : ('s, _ , 'a, _ ) lens -> ('s -> 'a)
+val set     : ('s, 't, _ , 'b) lens -> ('s -> 'b -> 't)
+val over    : ('s, 't, 'a, 'b) lens -> f:('a -> 'b) -> ('s -> 't)
 val compose : ('ss, 'tt, 's, 't) lens
               -> ('s, 't, 'a, 'b) lens
               -> ('ss, 'tt, 'a, 'b) lens
