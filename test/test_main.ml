0558c2099bcfde40a26de116056c4bb2872d58be
Milo Turner <iitalics@gmail.com>
2018-07-07 19:13:07 -0700

    Flatten module structure of Lenslib

--- test/test_main.ml
+++ test/test_main.ml
@@ -1,5 +1,5 @@
+open Lenslib.Infix
 open Lenslib
-open Lenslib.Lens.Infix
 
 module Posn = struct
   type t =
@@ -36,7 +36,7 @@ let () =
        ; "_1" >::
            begin fun ctxt ->
            assert_equal ~ctxt 5 ((5, 3) ^. _1) ;
-           assert_equal ~ctxt (false, 3) (Lens.set _1 (5, 3) false) ;
+           assert_equal ~ctxt (false, 3) (set _1 (5, 3) false) ;
            end
 
        ; "dmg" >::
@@ -51,7 +51,7 @@ let () =
            let tup1 = (3, 5),   "a" in
            let tup2 = (3, "5"), "a" in
            assert_equal ~ctxt tup2
-             (Lens.over (_1 ^> _2) tup1
+             (over (_1 ^> _2) tup1
                 ~f:string_of_int) ;
            end
 
@@ -60,6 +60,6 @@ let () =
            let p1 = { pl_pos = { x = 0 ; y = 0 } ; pl_hp = 10 } in
            let p2 = { pl_pos = { x = 2 ; y = 0 } ; pl_hp = 10 } in
            assert_equal ~ctxt p2
-             (Lens.set (_player_pos ^> Posn._x)
+             (set (_player_pos ^> Posn._x)
                 p1 2) ;
            end ]
